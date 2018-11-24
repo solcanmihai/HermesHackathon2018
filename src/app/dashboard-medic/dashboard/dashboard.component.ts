1be8a0ce0e625b138c01de40c2bcc2ca6a9223ed
Alex Benchea <alexbenchea99@yahoo.com>
2018-11-24 23:26:03 +0200

    send location medic

--- src/app/dashboard-medic/dashboard/dashboard.component.ts
+++ src/app/dashboard-medic/dashboard/dashboard.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { interval, Subscription } from 'rxjs';
+import { Socket } from 'ngx-socket-io';
+import { AuthService } from '../../services/auth.service';
+
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
 
-  constructor() { }
+
+export class DashboardComponent implements OnInit {
+  lat = null;
+  long = null;
+  subscription: Subscription;
+
+  
+  constructor(
+    private dataService: DataService,
+    private socket: Socket,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    const source = interval(10000/2);
+    this.subscription = source.subscribe(val => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.lat = position.coords.latitude
+          this.long = position.coords.longitude
+          this.sendMessage()
+        });
+      } else {
+        alert("Geolocation is not supported by this browser.");
+      }
+    }
+
+      
+    );
+
+
+    
   }
 
+  sendMessage(){
+    let body = {
+      lat: this.lat,
+      long: this.long,
+      token: this.authService.getToken()
+    }
+
+    this.socket.emit("location", body);
+    console.log(body)
+  }
 }

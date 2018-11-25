91d21b845032a00e5e37d045be18be473c8a3b0c
Solcan Mihai <solcan.mihaiandrei@yahoo.com>
2018-11-25 11:34:09 +0200

    login

--- src/app/landing/login/login.component.ts
+++ src/app/landing/login/login.component.ts
@@ -18,6 +18,24 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
+  blurMail(event){
+    if(this.email == ''){
+      event.target.classList.remove('ui-state-filled');
+    }
+    else{
+      event.target.classList.add('ui-state-filled');
+    }
+  }
+
+  blurPass(event){
+    if(this.password == ''){
+      event.target.classList.remove('ui-state-filled');
+    }
+    else{
+      event.target.classList.add('ui-state-filled');
+    }
+  }
+
   handleLogin(){
     console.log('Hello')
     this.authService.login(this.email, this.password);
